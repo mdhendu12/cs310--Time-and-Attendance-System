a939de53a4b246ac3c5e4495c782733ad07bd299
Matthew Henderson <mhenderson7@stu.jsu.edu>
2022-04-04 16:39:24 -0500

    Feature passes initial tests, waiting for new tests to be implemented.

--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -1,7 +1,9 @@
 package edu.jsu.mcis.cs310.tas_sp22;
 
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import org.json.simple.*; 
 
 public class TAS {
@@ -13,6 +15,24 @@ public class TAS {
     }
           
     public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist) {
-        return "";
+        ArrayList<HashMap<String, String>> jsonData = new ArrayList<HashMap<String, String>>();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("E MM/dd/yyyy HH:mm:ss");
+        
+        for (Punch p : dailypunchlist) {
+                    
+            HashMap<String, String> punchData = new HashMap<>();
+            punchData.put("id", String.valueOf(p.getId()));
+            punchData.put("badgeid", String.valueOf(p.getBadgeid()));
+            punchData.put("terminalid", String.valueOf(p.getTerminalid()));
+            punchData.put("punchtype", String.valueOf(p.getPunchtype().toString()));
+            punchData.put("adjustmenttype", String.valueOf(p.getAdjustmenttype()));
+            punchData.put("originaltimestamp", String.valueOf(p.getOriginalTimestamp().format(dtf).toUpperCase()));
+            punchData.put("adjustedtimestamp", String.valueOf(p.getAdjustedTS().format(dtf).toUpperCase()));
+            jsonData.add(punchData);
+        }
+        
+        String json = JSONValue.toJSONString(jsonData);
+        
+        return json;
     }
 }

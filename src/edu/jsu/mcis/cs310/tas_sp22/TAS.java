6920c838bf10ce8f8edf6023c71fc6918852a618
Matthew Henderson <mhenderson7@stu.jsu.edu>
2022-04-04 14:47:34 -0500

    Added Feature6 test file, and added skeleton for 'getPunchListAsJSON' to TAS.java

--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -1,5 +1,9 @@
 package edu.jsu.mcis.cs310.tas_sp22;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.json.simple.*; 
+
 public class TAS {
     
     public static void main(String[] args) {
@@ -7,5 +11,8 @@ public class TAS {
         System.out.println("Hello World");
         
     }
-            
+          
+    public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist) {
+        return "";
+    }
 }

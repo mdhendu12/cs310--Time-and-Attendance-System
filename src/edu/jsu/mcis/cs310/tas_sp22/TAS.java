1b09cfe63bcb8c76c07d9781f3ec9b6e2d400d6d
Hunter Wert <hwert@stu.jsu.edu>
2022-04-11 13:19:29 -0500

    started calculateAbseenism

--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -1,10 +1,15 @@
 package edu.jsu.mcis.cs310.tas_sp22;
+import java.util.ArrayList;
 
 public class TAS {
+    public static double calculateAbseenism(ArrayList<Punch> pl, Shift S)
+        {
+            return b; 
+        }
     
     public static void main(String[] args) {
         
-        System.out.println("Hello World");
+        System.out.println("@everyone");
         
     }
             

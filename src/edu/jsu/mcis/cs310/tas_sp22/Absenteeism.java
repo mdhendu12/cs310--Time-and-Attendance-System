9e2711f91081a3f367dcdadcf54a526b17d65bee
Hunter Wert <hwert@stu.jsu.edu>
2022-04-10 20:26:01 -0500

    Worked on getPayPeriodPunchList

--- src/edu/jsu/mcis/cs310/tas_sp22/Absenteeism.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/Absenteeism.java
@@ -1,5 +1,40 @@
 package edu.jsu.mcis.cs310.tas_sp22;
+import java.time. *; 
 
 public class Absenteeism {
+    private String badgeid; 
+    private LocalDate payPeriod; 
+    private double percentage; 
+    
+    public Absenteeism (String string, LocalDate date, double percent)
+    {
+        this.badgeid = string; 
+        this.payPeriod = date;
+        this.percentage = percent; 
+    }
+    
+    public String getBadgeid()
+    {
+        return badgeid; 
+    }
+    
+    public LocalDate getPayPeriod()
+    {
+        return payPeriod; 
+    }
+    
+    public double getPercentage()
+    {
+        return percentage; 
+    }
+    
+    @Override 
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder(); 
+        sb.append("#").append(badgeid).append(" (Pay Period Starting ").append(payPeriod).append("): ");
+        sb.append(percentage).append("%"); 
+        return sb.toString(); 
+    }
     
 }

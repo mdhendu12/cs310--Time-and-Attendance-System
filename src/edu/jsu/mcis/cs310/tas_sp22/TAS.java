ee5f77f591b8e81c363c695c8fbbc7f0925f3ea1
Kendall McCay <kmccay2@stu.jsu.edu>
2022-04-14 23:41:19 -0500

    Refactored feature 5.

--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -6,6 +6,40 @@ import org.junit.Before;
 public class TAS {
     
     public static void main(String[] args) {
+  
+    }
+    
+    protected static boolean clockedOutForLunch (Punch firstPunch, Punch secondPunch, Shift shift) {
+        
+        boolean clockedOut;
+        
+        if (secondPunch.getAdjustmenttype().equals("Lunch Start")) {
+            clockedOut = true;
+        }
+            
+        else if (firstPunch.getAdjustmenttype().equals("Lunch Stop")) {
+            clockedOut = true;
+        }
+
+        else {clockedOut = false;}
+        
+        return clockedOut;
+
+}
+    
+    protected static boolean punchTypesCorrect (Punch firstPunch, Punch secondPunch, Shift shift) {
+        
+        boolean correct = false;
+        String first = firstPunch.getPunchtype().toString();
+        String second = secondPunch.getPunchtype().toString();
+        
+        if (first != "CLOCK IN" || second != "CLOCK OUT") {
+                    correct = false;
+                }
+        
+        else {correct = true;}
+        
+        return correct;
         
     }
     
@@ -14,59 +48,65 @@ public class TAS {
         int totalMinutes = 0;
         Punch firstPunch = null;
         Punch secondPunch = null;
-        boolean clockout = true;
+        boolean correct = false;
+        boolean clockedOut;
         Duration minutes;
         Iterator<Punch> it = dailypunchlist.iterator();
         
         if (dailypunchlist.size() > 1) {
             
             while (it.hasNext()) {
-
-                if (clockout) {
-                    firstPunch = it.next();
-                }
-
-                else {
-                    clockout = true;
-                }
-
-                if (it.hasNext()) {secondPunch = it.next();}
-                else {break;}
                 
-                if (secondPunch.getPunchtype().toString() == "CLOCK IN") {
+                if (firstPunch == null && secondPunch == null) {
+                    firstPunch = it.next();
+                    secondPunch = it.next();
+                }
+                
+                else if ((firstPunch == secondPunch) && it.hasNext()) {secondPunch = it.next();}
+
+                else {
+                    if (it.hasNext()) {firstPunch = it.next();}
+                    if (it.hasNext()) {secondPunch = it.next();}
+                    else {break;}
+                }
+
+                correct = punchTypesCorrect(firstPunch, secondPunch, shift);
+
+                if (!correct) {
                     firstPunch = secondPunch;
-                    clockout = false;
+                    clockedOut = true;
                 }
 
                 else {
-
-                    if (secondPunch.toString() == "TIME OUT") {break;}
+                    clockedOut = clockedOutForLunch(firstPunch, secondPunch, shift);
+                    correct = false;
+                }
+                
+                String day = secondPunch.getOriginalTimestamp().getDayOfWeek().toString();
+        
+                if (day != "SATURDAY" && day != "SUNDAY") {
                     
-                    else {
+                    if (clockedOut) {
                         minutes = Duration.between(firstPunch.getAdjustedTS(), secondPunch.getAdjustedTS());
                         totalMinutes += minutes.toMinutes();
                     }
 
+                    else {
+                        minutes = Duration.between(firstPunch.getAdjustedTS(), secondPunch.getAdjustedTS());
+                        totalMinutes += (minutes.toMinutes() - shift.getLunchduration().toMinutes());
+                    }
+
                 }
+                
+                else {
+                    minutes = Duration.between(firstPunch.getAdjustedTS(), secondPunch.getAdjustedTS());
+                    totalMinutes += minutes.toMinutes();
                 }
+            }
         }
         
-        else {return totalMinutes;}
-
-        if (secondPunch.getOriginalTimestamp().getDayOfWeek().toString() != "SATURDAY" && secondPunch.getOriginalTimestamp().getDayOfWeek().toString() != "SUNDAY") {
-            
-            if (totalMinutes > shift.getLunchthreshold() && secondPunch.getAdjustmenttype() == "Lunch Start") {
-                return totalMinutes - shift.getLunchduration().toMinutesPart();
-            }
-            
-            else if (totalMinutes > shift.getLunchthreshold() && (!firstPunch.getAdjustmenttype().equals("Lunch Stop")) && (!secondPunch.getAdjustmenttype().equals("Lunch Start"))) {
-                return totalMinutes - shift.getLunchduration().toMinutesPart();
-            }
-
-            else {return totalMinutes;}
-            }
-
-        else {return totalMinutes;}
+        return totalMinutes;
+        
         }
 }
 
